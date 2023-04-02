b39da449ff1100ca35e4f1803a0a85d283797f46
novakvova <novakvova@gmail>
2023-04-02 11:12:27 +0300

    network security upload image localhost and webserver

--- Porject/app/src/main/java/com/example/myshop/MainActivity.java
+++ Porject/app/src/main/java/com/example/myshop/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.myshop.application.HomeApplication;
+import com.example.myshop.constants.Urls;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView avatar = (ImageView) findViewById(R.id.iv_avatar);
+        String url= Urls.BASE+"/images/3.jpg";
+        Glide.with(HomeApplication.getAppContext())
+                .load(url)
+                .apply(new RequestOptions().override(600))
+                .into(avatar);
     }
     
     public void onClickBtn(View view) {
