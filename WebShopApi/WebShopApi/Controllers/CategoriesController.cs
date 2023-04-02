31de7afb0472ff5d061c5ceec91cf63e693afc27
novakvova <novakvova@gmail>
2023-04-02 13:05:48 +0300

    Show data in api list categories

---
+++ WebShopApi/WebShopApi/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShopApi.Data;
+using WebShopApi.Models;
+
+namespace WebShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly AppEFContext _appEFContext;
+
+        public CategoriesController(IMapper mapper, AppEFContext appEFContext)
+        {
+            _mapper = mapper;
+            _appEFContext = appEFContext;
+        }
+
+        [HttpGet("list")]
+        public async Task<IActionResult> Get()
+        {
+            var model = await _appEFContext.Categories
+                .Where(x=>x.isDelete == false)
+                .OrderBy(x=>x.Priority)
+                .Select(x=>_mapper.Map<CategoryItemViewModel>(x))
+                .ToListAsync();
+            return Ok(model);
+        }
+    }
+}
--- WebShopApi/WebShopApi/Program.cs
+++ WebShopApi/WebShopApi/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using WebShopApi.Data;
+using WebShopApi.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppEFContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(AppMapProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,5 +38,7 @@ app.UseStaticFiles(new StaticFileOptions
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.SeedData();
 
 app.Run();
