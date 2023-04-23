d4f3b26443977a3bd2bd2f3e980ceb2eda9c01b4
novakvova <novakvova@gmail>
2023-04-23 13:14:39 +0300

    Working working android app

--- Porject/app/src/main/java/com/example/myshop/ChangeImageActivity.java
+++ Porject/app/src/main/java/com/example/myshop/ChangeImageActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import com.oginotihiro.cropview.CropUtil;
 import com.oginotihiro.cropview.CropView;
+
+import java.io.File;
 
 public class ChangeImageActivity extends AppCompatActivity {
 
@@ -29,7 +33,6 @@ public class ChangeImageActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == RESULT_LOAD_IMAGE) {
             if(resultCode == RESULT_OK && data!=null){
                 Uri selectedImage = data.getData();
@@ -40,14 +43,27 @@ public class ChangeImageActivity extends AppCompatActivity {
     } //onActivityResult
 
     public void RotateRightImage(View view) {
-
+        cropView.setRotation(cropView.getRotation()+90);
     }
 
     public void RotateLeftImage(View view) {
-
+        cropView.setRotation(cropView.getRotation()-90);
     }
 
     public void ChangeImage(View view) {
+        String fileTemp = java.util.UUID.randomUUID().toString();
+        Bitmap croppedBitmap = cropView.getOutput();
+        Matrix matrix = new Matrix();
+        matrix.postRotate(cropView.getRotation());
+        Bitmap rotatedBitmap = Bitmap.createBitmap(croppedBitmap, 0, 0, croppedBitmap.getWidth(), croppedBitmap.getHeight(), matrix, true);
 
+        Uri fileSavePath = Uri.fromFile(new File(getCacheDir(), fileTemp));
+        CropUtil.saveOutput(this, fileSavePath, rotatedBitmap, 90);
+
+        //Вертаємо результат - Шлях до файлу, який збережено через кропер
+        Intent intent = new Intent();
+        intent.putExtra("croppedUri", fileSavePath);
+        setResult(300, intent);
+        finish();
     }
 }
